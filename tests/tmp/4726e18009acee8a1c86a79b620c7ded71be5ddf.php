2e4bdc5a0dc021e66bc8a0e1f8827ac7a9942033
César D. Rodas <crodas@php.net>
2010-07-15 17:42:48 -0400

    - Added tests for nested forloop operations

---
+++ tests/tmp/4726e18009acee8a1c86a79b620c7ded71be5ddf.php
@@ -0,0 +1,81 @@
+<?php
+/* Generated from ./assert_templates/loop.tpl */
+function haanga_4726e18009acee8a1c86a79b620c7ded71be5ddf($vars, $return=FALSE, $blocks=array())
+{
+    extract($vars);
+    $buffer1  = "";
+    $psize_1  = count($array);
+    $revcount0_1  = $psize_1 - 1;
+    foreach ($array as  $i) {
+        $buffer1 .= "\n    ".$revcount0_1."\n";
+        $revcount0_1  = $revcount0_1 - 1;
+    }
+    $buffer1 .= "\n\n";
+    $psize_1  = count($array);
+    $revcount_1  = $psize_1;
+    foreach ($array as  $i) {
+        $buffer1 .= "\n    ".$revcount_1."\n";
+        $revcount_1  = $revcount_1 - 1;
+    }
+    $buffer1 .= "\n\n";
+    $forcounter1_1  = 1;
+    $psize_1  = count($array);
+    $islast_1  = $forcounter1_1 == $psize_1;
+    foreach ($array as  $i) {
+        $buffer2  = "\n    ";
+        if ($islast_1) {
+            $buffer2 .= " Last ".$i;
+        }
+        $buffer2 .= "\n\n";
+        $buffer1 .= trim($buffer2);
+        $forcounter1_1++;
+        $islast_1  = $forcounter1_1 == $psize_1;
+    }
+    $buffer1 .= "\n\n";
+    $forcounter1_1  = 1;
+    $psize_1  = count($array_nested);
+    $islast_1  = $forcounter1_1 == $psize_1;
+    foreach ($array_nested as  $k => $sub) {
+        $buffer2  = "\n\n    ";
+        foreach ($sub as  $arr) {
+            $buffer2 .= "\n        ";
+            foreach ($arr as  $val) {
+                $buffer2 .= "\n            ";
+                if ($islast_1) {
+                    $buffer2 .= " Last ".$k;
+                }
+                $buffer2 .= "\n        ";
+            }
+            $buffer2 .= "\n    ";
+        }
+        $buffer2 .= "\n\n";
+        $buffer1 .= trim($buffer2);
+        $forcounter1_1++;
+        $islast_1  = $forcounter1_1 == $psize_1;
+    }
+    $buffer1 .= "\n\n";
+    $isfirst_1  = TRUE;
+    foreach ($array_nested as  $k => $sub) {
+        $buffer2  = "\n\n    ";
+        foreach ($sub as  $arr) {
+            $buffer2 .= "\n        ";
+            foreach ($arr as  $val) {
+                $buffer2 .= "\n            ";
+                if ($isfirst_1) {
+                    $buffer2 .= " first ".$k;
+                }
+                $buffer2 .= "\n        ";
+            }
+            $buffer2 .= "\n    ";
+        }
+        $buffer2 .= "\n\n";
+        $buffer1 .= trim($buffer2);
+        $isfirst_1  = FALSE;
+    }
+    $buffer1 .= "\n";
+    if ($return == TRUE) {
+        return $buffer1;
+    } else {
+        echo $buffer1;
+    }
+}
--- tests/tmp/e3288a8c38d2925df1b81c50c72b7eee31f8c2f9.php
+++ tests/tmp/e3288a8c38d2925df1b81c50c72b7eee31f8c2f9.php
@@ -15,33 +15,33 @@ function haanga_e3288a8c38d2925df1b81c50c72b7eee31f8c2f9($vars, $return=FALSE, $
     }
     /* Sorting done */
     $buffer1 .= "\n\n";
-    $forcounter1_6  = 1;
+    $forcounter1_1  = 1;
     foreach ($sorted_users as  $user) {
         $buffer1 .= "\n    ".htmlentities($user["grouper"])."\n    ";
-        $forcounter1_7  = 1;
-        $psize_7  = count($user["list"]);
-        $islast_7  = $forcounter1_7 == $psize_7;
-        $isfirst_7  = TRUE;
-        $revcount_7  = $psize_7;
-        $revcount0_7  = $psize_7 - 1;
+        $forcounter1_2  = 1;
+        $psize_2  = count($user["list"]);
+        $islast_2  = $forcounter1_2 == $psize_2;
+        $isfirst_2  = TRUE;
+        $revcount_2  = $psize_2;
+        $revcount0_2  = $psize_2 - 1;
         foreach ($user["list"] as  $u) {
-            $buffer1 .= "\n        ".$forcounter1_7."-".$revcount_7."-".$revcount0_7." (".$forcounter1_6."). ".htmlentities(ucfirst($u["name"]))." (";
-            if ($isfirst_7) {
+            $buffer1 .= "\n        ".$forcounter1_2."-".$revcount_2."-".$revcount0_2." (".$forcounter1_1."). ".htmlentities(ucfirst($u["name"]))." (";
+            if ($isfirst_2) {
                 $buffer1 .= "first";
             } else {
-                if ($islast_7) {
+                if ($islast_2) {
                     $buffer1 .= "last";
                 }
             }
             $buffer1 .= ")\n    ";
-            $forcounter1_7++;
-            $islast_7  = $forcounter1_7 == $psize_7;
-            $isfirst_7  = FALSE;
-            $revcount_7  = $revcount_7 - 1;
-            $revcount0_7  = $revcount0_7 - 1;
+            $forcounter1_2++;
+            $islast_2  = $forcounter1_2 == $psize_2;
+            $isfirst_2  = FALSE;
+            $revcount_2  = $revcount_2 - 1;
+            $revcount0_2  = $revcount0_2 - 1;
         }
         $buffer1 .= "\n";
-        $forcounter1_6++;
+        $forcounter1_1++;
     }
     $buffer1 .= "\n";
     if ($return == TRUE) {
