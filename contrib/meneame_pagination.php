8b2c4c52d5fb95375c082e5e3f9381de460157b0
César D. Rodas <crodas@php.net>
2010-07-19 17:20:41 -0400

    - Meneame pagination (contrib is working)

--- contrib/meneame_pagination.php
+++ contrib/meneame_pagination.php
@@ -7,18 +7,37 @@ class Meneame_Pagination_Tag
     function generator($cmp, $args, $redirected)
     {
         if (count($args) != 3 && count($args) != 4) {
-            throw new Haanga_CompilerException("Memeame_Pagination requires 3 parameters");
+            throw new Haanga_CompilerException("Memeame_Pagination requires 3 or 4 parameters");
         }
         if (count($args) == 3) {
             $args[3] = $cmp->expr_number(5);
         }
-        $one   = $cmp->expr_number(1);
-        $out   = array();
-        $out[] = $cmp->op_declare('mnm_total', $cmp->expr_exec('ceil', $cmp->expr('/', $args[2], $args[1]))); 
-        $out[] = $cmp->op_declare('mnm_start', $cmp->expr_exec('max', $cmp->expr('-', $args[0], $cmp->expr_exec('intval', $args[3])), $one));
-        $out[] = $cmp->op_declare('mnm_end', $cmp->expr('+', $cmp->expr_var('mnm_start'), $cmp->expr('-', $args[3], $one)));
 
-        return new ArrayIterator($out);
+        $current = $cmp->expr_var('mnm_current');
+        $total   = $cmp->expr_var('mnm_total');
+        $start   = $cmp->expr_var('mnm_start');
+        $end     = $cmp->expr_var('mnm_end');
+        $zero    = $cmp->expr_number(0);
+        $one     = $cmp->expr_number(1);
+        $two     = $cmp->expr_number(2);
+        $false   = $cmp->expr_FALSE();
+        
+
+        $code   = array();
+        $code[] = $cmp->op_declare($current, $args[0]);
+        $code[] = $cmp->op_declare($total, $cmp->expr_exec('ceil', $cmp->expr('/', $args[2], $args[1]))); 
+        $code[] = $cmp->op_declare($start, $cmp->expr_exec('max', $cmp->expr('-', $current, $cmp->expr_exec('intval', $cmp->expr('/',$args[3], $two))), $one));
+        $code[] = $cmp->op_declare($end, $cmp->expr('+', $start, $cmp->expr('-', $args[3], $one)));
+        $code[] = $cmp->op_declare('mnm_prev', $cmp->expr_cond($cmp->expr('==', $one, $current), $false, $cmp->expr('-', $current, $one)));
+        $code[] = $cmp->op_declare('mnm_next', $cmp->expr_cond(
+            $cmp->expr('||', $cmp->expr('<', $args[2], $zero), $cmp->expr('<', $current, $total)),
+            $cmp->expr('+', $current, $one),
+            $false));
+
+        $code[] = $cmp->op_declare('mnm_pages', $cmp->expr_exec('range', $start, $cmp->expr_cond($cmp->expr('<', $end, $total), $end, $total)));
+        //$code[] = $cmp->op_foreach
+
+        return new ArrayIterator($code);
     }
 
 }
