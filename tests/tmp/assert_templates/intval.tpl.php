0d76c31d79e73302bb0ea7ee31dd5bb4899d9f5f
César D. Rodas <crodas@php.net>
2010-08-14 17:54:54 -0400

    - Added intval tests

---
+++ tests/tmp/assert_templates/intval.tpl.php
@@ -0,0 +1,16 @@
+<?php
+$HAANGA_VERSION  = '1.1.3';
+/* Generated from /home/crodas/projects/playground/haanga/tests/assert_templates/intval.tpl */
+function haanga_97d0fec2255e31f51478c5d222c422dc247322f7($vars, $return=FALSE, $blocks=array())
+{
+    global $test_global;
+    extract($vars);
+    if ($return == TRUE) {
+        ob_start();
+    }
+    echo htmlentities(intval($float)).'
+';
+    if ($return == TRUE) {
+        return ob_get_clean();
+    }
+}
