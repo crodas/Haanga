7435de1be18d9da0012a7a679cd97f572f094591
César D. Rodas <crodas@php.net>
2010-09-17 01:11:03 -0400

    Fixed warning issues in dynamic variables (ie: variable[another.property]), reported by @gallir

--- tests/tmp/assert_templates/if_else_simple.tpl.php
+++ tests/tmp/assert_templates/if_else_simple.tpl.php
@@ -22,6 +22,48 @@ function haanga_e7733880179030f69680f92d682d991e9f93c62e($vars, $return=FALSE, $
     }
     echo '
 ';
+    if (1 === 2 || 1 + 2 === 3) {
+        echo 'True';
+    } else {
+        echo 'False';
+    }
+    echo '
+';
+    if (1 !== 2) {
+        echo 'True';
+    } else {
+        echo 'False';
+    }
+    echo '
+';
+    if (1 > 2) {
+        echo 'True';
+    } else {
+        echo 'False';
+    }
+    echo '
+';
+    if (1 >= 2) {
+        echo 'True';
+    } else {
+        echo 'False';
+    }
+    echo '
+';
+    if (1 <= 2) {
+        echo 'True';
+    } else {
+        echo 'False';
+    }
+    echo '
+';
+    if (1 < 2) {
+        echo 'True';
+    } else {
+        echo 'False';
+    }
+    echo '
+';
     if ($return == TRUE) {
         return ob_get_clean();
     }
