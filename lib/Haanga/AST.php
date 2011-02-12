3beb59d3e6f080f18f202790b2aecf90630ac690
César D. Rodas <crodas@php.net>
2011-02-12 03:34:01 -0300

    + Added list of Classes for nodes, time to clean up the code a little bit :-)

--- lib/Haanga/AST.php
+++ lib/Haanga/AST.php
@@ -35,6 +35,185 @@
   +---------------------------------------------------------------------------------+
 */
 
+class Haanga_Generator_PHP {
+    public function string($args) {
+        return  "'" . addslashes($args[0]) . "'";
+    }
+
+    public function operator($args) {
+        return $args[0];
+    }
+
+    public function property($args) {
+        return "->" . $args[0];
+    }
+
+    public function assign ($args) {
+        return $args[0] . '=' . $args[1];
+    }
+
+    public function expr($args) {
+        $prev  = null;
+        $code  = '';
+        foreach ($args as $val) {
+            if ($prev && $val->getType() == $prev->getType() && $val->getType()  == "String") {
+                $code .= '.';
+            }
+            $code .= $val;
+            $prev  = $val;
+        }
+        return $code;
+    }
+
+    public function number($args) {
+        return (string)$args[0];
+    }
+
+    public function variable($args) {
+        foreach ($args as $value) {
+            if (empty($var)) {
+                $var = '$' . $value;
+            } else {
+                if ($value InstanceOf Haanga_Node_Property) {
+                    $var .= $value;
+                } else {
+                    $var .= "[" .  $value  . "]";
+                }
+            }
+        }
+        return $var;
+    }
+}
+
+
+/**
+ *  
+ *
+ */
+abstract class Haanga_Node 
+{
+    protected $line;
+    protected $attrs;
+    protected $nodes;
+
+    protected static $generator;
+
+    public function __construct(array $nodes, Array $attrs=array(), $line=0) {
+        $this->nodes = $nodes;
+        $this->attrs = $attrs;
+        $this->line  = $line;
+    }
+
+    public function getAttributes() {
+        return $this->attrs;
+    }
+
+    public function getType() {
+        return substr(get_class($this), strlen(__CLASS__) +1);
+    }
+
+    public static function setGenerator($generator) {
+        self::$generator = $generator;
+    }
+
+
+    final public function __toString() {
+        $class = $this->getType();
+        $callback = array(self::$generator, $class);
+        if (is_callable($callback)) {
+            return call_user_func($callback, $this->getAttributes());
+        } else {
+            throw new Exception("Don't know how to generate {$class}");
+        }
+    }
+
+    public static function convertNative($value) {
+        if ($value instanceOf Haanga_Node) {
+            return $value;
+        } 
+        
+        switch ($value) {
+        case '+': case '++': case '-': 
+        case '*': case '/':case '%':
+            return new Haanga_Node_Operator($value);
+        }
+        
+        if (is_string($value)) {
+            $value = new Haanga_Node_String($value);
+        } else if (is_numeric($value)) {
+            $value = new Haanga_Node_Number($value);
+        } else {
+            throw new Exception("Don't know how to convert {$value}");
+        }
+        return $value;
+    }
+}
+
+Haanga_Node::setGenerator(new Haanga_Generator_PHP);
+
+abstract class Haanga_Node_Basic extends Haanga_Node {
+    public function __construct($text, $line=0) {
+        parent::__construct(array(), array($text), $line);
+    }
+}
+
+final class Haanga_Node_Number extends Haanga_Node_Basic { }
+
+final class Haanga_Node_String extends Haanga_Node_Basic { }
+
+final class Haanga_Node_Operator extends Haanga_Node_Basic { }
+
+final class Haanga_Node_Property extends Haanga_Node {
+    public function __construct($name, $line=0) {
+        parent::__construct(array(), array($name), $line);
+    }
+}
+
+
+final class Haanga_Node_Variable extends Haanga_Node {
+    public function __construct($def="", $line=0) {
+        if (is_string($def)) {
+            $def = array($def);
+        } else {
+            foreach ($def as $id => $value) {
+                if ($id == 0 || $value InstanceOf Haanga_Node) {
+                    continue;
+                }
+
+                $value = Haanga_Node::convertNative($value);
+            }
+        }
+        parent::__construct(array(),  $def, $line);
+    }
+}
+
+final class Haanga_Node_Assign extends Haanga_Node {
+    public function __construct(Haanga_Node_Variable $a, Haanga_Node_Expr $b, $line=0) {
+        parent::__construct(array(), array($a, $b), $line);
+    }
+}
+
+
+final class Haanga_Node_Expr extends Haanga_Node {
+    function __construct(array $expr, $line=0) {
+        foreach ($expr as &$value) {
+            if (!$value instanceof Haanga_Node) {
+                $value = Haanga_Node::convertNative($value);
+            }
+        }
+        parent::__construct(array(), $expr, $line);
+    }
+}
+
+
+$def = array('cesar', new Haanga_Node_String('rodas'),  new Haanga_Node_Property('cesar'), 'david', 5, 9.99);
+$var = new Haanga_Node_Variable($def);
+
+$expr = new Haanga_Node_Expr(array(5, '+', 9));
+$assign1 = new Haanga_Node_Assign($var, $expr);
+$assign2 = new Haanga_Node_Assign($var, new Haanga_Node_Expr(array('cesar', 'rodas')));
+var_dump((string) $assign1, (String)$assign2);exit;
+
 class Haanga_New_AST
 {
     public $block = array();
@@ -367,7 +546,6 @@ class Haanga_AST
         } else if ($obj === NULL) {
             $value = array();
         } else {
-            var_Dump($obj);
             throw new Exception("Imposible to get the value of the object");
         }
     }
