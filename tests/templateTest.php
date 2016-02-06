77ddd952ebc1576211c8fe996dc62aedb5a2fcd8
Cesar Rodas <crodas@php.net>
2016-02-06 04:02:55 -0300

    Fixed phpunit tests

--- tests/templateTest.php
+++ tests/templateTest.php
@@ -43,6 +43,7 @@ class templateTest extends PHPUnit_Framework_TestCase
         $this->init($test_file, $expected);
         $output = Haanga::Load($test_file, $data, TRUE);
         $this->assertEquals($output, $expected);
+        $this->assertTrue(filemtime(__DIR__ . $test_file) <= filemtime(__DIR__ . '/tmp/assert_templates/' . basename($test_file) . '.php'));
     }
 
     /** 
