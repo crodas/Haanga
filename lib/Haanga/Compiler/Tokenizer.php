2619bb5f883a22f3ab7ad8d59e728f17231cd44f
Viktor Zhyliaiev <gilyaev@gmail.com>
2017-06-01 16:45:37 +0200

    Deleted the strange operation with file system in class the Haanga_Compiler_Tokenizer

--- lib/Haanga/Compiler/Tokenizer.php
+++ lib/Haanga/Compiler/Tokenizer.php
@@ -558,7 +558,6 @@ class Haanga_Compiler_Tokenizer
     static function init($template, $compiler, $file='')
     {
         $lexer  = new Haanga_Compiler_Tokenizer($template, $compiler, $file);
-        file_put_contents('/tmp/foo.php', $file . "\n", FILE_APPEND);
         $parser = new Haanga_Compiler_Parser($lexer, $file);
 
         $parser->compiler = $compiler;
